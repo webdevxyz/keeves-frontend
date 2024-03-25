683da4d535e5b830db361f3ad162b6fb54cc7f6f
Chandrakanta Behera <chandan16460@gmail.com>
2024-03-25 15:47:50 +0530

    home page section added

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,56 @@
 <template>
-    <div>
-        Footer
-    </div>
+    <footer>
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-branding">
+                    <img src="/images/logo.svg" alt="Logo" class="footer-logo">
+                </div>
+                <div class="footer-navigation">
+                    <nav aria-label="Footer navigation">
+                        <ul class="footer-menu">
+                            <li class="menu-title">Get to Know Us</li>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Press Releases</a></li>
+                            <li><a href="#">MoMarket Science</a></li>
+                        </ul>
+                        <ul class="footer-menu">
+                            <li class="menu-title">Connect with Us</li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Instagram</a></li>
+                        </ul>
+                        <ul class="footer-menu">
+                            <li class="menu-title">Make Money with Us</li>
+                            <li><a href="#">Sell on Mo Market</a></li>
+                            <li><a href="#">Sell under Mo Market Accelerator</a></li>
+                            <li><a href="#">Protect & Build your Brand</a></li>
+                            <li><a href="#">Fulfilment by Mo Market</a></li>
+                            <li><a href="#">Advertise your Products</a></li>
+                            <li><a href="#">MoMa Pay on Merchants</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="copyright">
+                    <a href="https://brandbook.studio/" target="_blank" rel="noopener noreferrer">© Mo Market 2023</a>
+                </div>
+                <div class="footer-links">
+                    <a href="#">Sitemap</a>
+                    <span class="divider">|</span>
+                    <a href="#">Robot.txt</a>
+                    <span class="divider">|</span>
+                    <a href="#">Disclaimer</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
 </template>
+
+<script setup>
+import "../css/footer.css"
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default ({
   css: [
-    '/css/global.css'
+    '/css/global.css',
+    '/css/app.css'
   ],
+
   devtools: { enabled: true }
-})
+});
